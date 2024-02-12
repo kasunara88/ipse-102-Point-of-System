53e20ebd49f0ed74ed2d740b9276a219e27b42f4
Kasun Aravinda <wmaravinda@gmail.com>
2024-02-13 02:22:47 +0500

    updated

--- pos/src/main/java/com/ijse/pos/entity/Item.java
+++ pos/src/main/java/com/ijse/pos/entity/Item.java
@@ -1,20 +1,17 @@
 package com.ijse.pos.entity;
 
 import java.util.HashSet;
-import java.util.List;
+
 import java.util.Set;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToMany;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.Setter;
--- pos/src/main/java/com/ijse/pos/entity/ItemCategory.java
+++ pos/src/main/java/com/ijse/pos/entity/ItemCategory.java
@@ -1,16 +1,9 @@
 package com.ijse.pos.entity;
 
-import java.util.List;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.Setter;
 
